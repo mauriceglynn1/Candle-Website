7c37dbbe6c7978e0d007b7044eee1657bfc74a89
Maurice <mauriceglynn1@gmail.com>
2025-10-07 20:17:44 -0500

    added styles folder

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import {Home} from './Pages/Home'
 import {Login} from './Pages/Login'
